f9b36c768180faa9d46e7c3d09c7482d1a0c1808
wenwen-39 <C109118205@nkust.edu.tw>
2021-01-03 23:41:36 +0800

    my first commit

--- wen/settings.py
+++ wen/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/staticroot'
+django_heroku.settings(locals())
